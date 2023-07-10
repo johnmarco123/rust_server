fed01ea75fd3d7ce7fdba7404d505b0bc53c4655
johnmarco123 <johnmarcotasillo@gmail.com>
2023-07-09 20:34:10 -0400

    WOP

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    let address = "127.0.0.1:80";
+    let address = "127.0.0.1:8000";
     let listener = TcpListener::bind(address).unwrap();
     // accept connections and process them serially
     println!("Listening on address {}", address);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        handle_connection(stream); 
     }
 }
 
@@ -21,13 +21,14 @@ fn read_image_file(path: &str) -> Result<Vec<u8>, std::io::Error> {
     Ok(buffer)
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader
+    fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+        let buf_reader = BufReader::new(&mut stream);
+    let request_line: String = buf_reader
         .lines()
         .next()
-        .unwrap()
-        .unwrap();
+        .unwrap()?;
+
+    println!("{:?}", request_line);
 
     let mut start_idx: usize = 0;
     let mut end_idx: usize = 0;
@@ -51,22 +52,26 @@ fn handle_connection(mut stream: TcpStream) {
         file_name = &requested_file[1..]
     };
 
-    if requested_file.ends_with(".jpg") || requested_file.ends_with(".png") {
-        let contents = read_image_file(file_name).unwrap();
-        let length = contents.len();
-        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: image/jpg\r\nContent-Length: {length}\r\n\r\n");
-        stream.write_all(response.as_bytes()).unwrap();
-        stream.write_all(&contents).unwrap();
-    } else {
-        let contents = fs::read_to_string(file_name);
-        let (status_line, contents) = match contents {
-            Ok(contents) => ("HTTP/1.1 200 OK", contents),
-            Err(_) => ("HTTP/1.1 404 NOT FOUND", String::from("File not found")),
-        };
+    if requested_file.ends_with(".jpg") ||
+        requested_file.ends_with(".png") ||
+            requested_file.ends_with(".ttf") ||
+            requested_file.ends_with(".ico")  {
+                //TODO Favicons do not work! why is this?
+                let contents: Vec<u8> = read_image_file(file_name)?;
+                let length = contents.len();
+                let response = format!("HTTP/1.1 200 OK\r\nContent-Type: image/x-icon\r\nContent-Length: {length}\r\n\r\n");
+                stream.write_all(response.as_bytes()).unwrap();
+                stream.write_all(&contents).unwrap();
+            } else {
+                let contents = fs::read_to_string(file_name);
+                let (status_line, contents) = match contents {
+                    Ok(contents) => ("HTTP/1.1 200 OK", contents),
+                    Err(_) => ("HTTP/1.1 404 NOT FOUND", String::from("File not found")),
+                };
 
-        let length = contents.len();
-        let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, length, contents);
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-
+                let length = contents.len();
+                let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, length, contents);
+                stream.write_all(response.as_bytes()).unwrap();
+            }
+    Ok(())
 }
